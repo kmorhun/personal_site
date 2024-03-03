31dc0c2894c48b063f52df0eca94041dea14e2b2
Kateryna Morhun <kmorhun@mit.edu>
2024-03-03 00:19:23 -0500

    Created a Project component

---
+++ src/lib/Project.svelte
@@ -0,0 +1,11 @@
+<article>
+    <h2>{p.title}</h2>
+    <img src={p.image} alt="">
+    <p>{p.description}</p>
+</article>
+
+<script>
+    // alias info to an internal variable
+    export let info = {};
+    let p = info;
+</script>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -4,17 +4,14 @@
 
 <script>
     import projects from '$lib/projects.json';
+    import Project from '$lib/Project.svelte';
 </script>
 
 <article class="content">
     <h1>What am I working on? At least {projects.length} things!</h1>
     <div class="projects">
         {#each projects as p}
-            <article>
-                <h2>{p.title}</h2>
-                <img src={p.image} alt="">
-                <p>{p.description}</p>
-            </article>
+            <Project info={p}/>
         {/each}
     </div>
 </article>
