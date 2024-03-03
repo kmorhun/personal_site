25a902eb5cdf515e654b8a022ce3ef72c1e51ab3
Kateryna Morhun <kmorhun@mit.edu>
2024-03-03 00:55:25 -0500

    moved nav bar to layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Nav from "../lib/Nav.svelte";
+
+    let pages = [
+        {url: "./", title: "Home"},
+        {url: "./projects", title: "Projects"},
+        {url: "./cv", title: "CV"},
+        {url: "./contact", title: "Contact"},
+        {url: "https://github.com/kmorhun", title: "Follow Me on Github~"},
+    ];
+</script>
+
+<Nav info={pages} />
+
+<slot />
--- static/global.js
+++ static/global.js
@@ -12,40 +12,40 @@ function $$ (selector, context = document) {
 // // add css styling class, as long as currentLink is not undefined
 // currentLink?.classList.add("current-page")
 
-let pages = {
-    ".": "Home",
-    "./projects": "Projects",
-    "./cv": "CV",
-    "./contact": "Contact",
-    "https://github.com/kmorhun": "Follow Me on Github~" ,
-}
+// let pages = {
+//     ".": "Home",
+//     "./projects": "Projects",
+//     "./cv": "CV",
+//     "./contact": "Contact",
+//     "https://github.com/kmorhun": "Follow Me on Github~" ,
+// }
 
 // const ARE_WE_HOME = document.documentElement.classList.contains("home");
 // console.log("ARE_WE_HOME", ARE_WE_HOME);
-let nav = document.createElement("nav");
-document.body.prepend(nav);
+// let nav = document.createElement("nav");
+// document.body.prepend(nav);
 
-for (let url in pages) {
-    let title = pages[url];
-    console.log("RAW URL", url);
+// for (let url in pages) {
+//     let title = pages[url];
+//     console.log("RAW URL", url);
 
-    // make the URL relative if you're not on the home page, or if deployed
-    // url = (ARE_WE_HOME || url.startsWith("https")) ? url : "../" + url;
-    // console.log("MODIFIED URL", url);
-    let a = document.createElement("a");
-    a.href = url;
-    a.textContent = title;
-    // console.log("a.host", a.host, "location.host", location.host, "a.pathname", a.pathname, "location.pathname", location.pathname)
-    if (a.host !== location.host) {
-        a.target = "_blank";
-    }
-    console.log(a.host, location.host, a.pathname, location.pathname)
-    if (a.host === location.host && a.pathname ===location.pathname) {
-        a.classList.add("current-page");
-    }
-    nav.append(a);
-    // nav.insertAdjacentHTML("beforeend", `<a href="${url}">${title}</a>`);
-}
+//     // make the URL relative if you're not on the home page, or if deployed
+//     // url = (ARE_WE_HOME || url.startsWith("https")) ? url : "../" + url;
+//     // console.log("MODIFIED URL", url);
+//     let a = document.createElement("a");
+//     a.href = url;
+//     a.textContent = title;
+//     // console.log("a.host", a.host, "location.host", location.host, "a.pathname", a.pathname, "location.pathname", location.pathname)
+//     if (a.host !== location.host) {
+//         a.target = "_blank";
+//     }
+//     console.log(a.host, location.host, a.pathname, location.pathname)
+//     if (a.host === location.host && a.pathname ===location.pathname) {
+//         a.classList.add("current-page");
+//     }
+//     nav.append(a);
+//     // nav.insertAdjacentHTML("beforeend", `<a href="${url}">${title}</a>`);
+// }
 
 // add the light/dark mode toggle
 document.body.insertAdjacentHTML("afterbegin", 
