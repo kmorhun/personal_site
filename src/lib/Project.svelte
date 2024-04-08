d4b100ac5e6b08e86b819d6b33e1acda341cab66
Kateryna Morhun <kmorhun@mit.edu>
2024-04-08 17:31:52 -0400

    added bikewatch to project page

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -1,6 +1,6 @@
 <article>
     <svelte:element this={"h"+hlevel}>{info.title}</svelte:element>
-    <img src={info.image} alt="">
+    <a href={info.url}><img src={info.image} alt=""></a>
     <div>
         <p>{info.description}</p>
         <p class="year">c. {info.year}</p>
